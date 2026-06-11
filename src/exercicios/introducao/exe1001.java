5f3d7f30cde4697a9e02dc78c01830a525d92843
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-11 11:29:52 -0300

    Exercicio 1001

---
+++ src/exercicios/introducao/exe1001.java
@@ -0,0 +1,14 @@
+package exercicios.introducao;
+
+import java.util.Scanner;
+
+public class exe1001 {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println("X = " + (a+b));
+    }
+}
