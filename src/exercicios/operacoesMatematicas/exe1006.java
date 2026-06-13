6f13750c3c306e257952e3b8be7db289b85d1560
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-12 21:02:25 -0300

    Exercicio 1006

---
+++ src/exercicios/operacoesMatematicas/exe1006.java
@@ -0,0 +1,17 @@
+package exercicios.operacoesMatematicas;
+
+import java.util.Scanner;
+
+public class exe1006 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double media = ((a*2) + (b*3) + (c*5)) / 10;
+
+        System.out.printf("MEDIA = %.1f%n", media);
+    }
+}
