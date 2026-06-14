5e556b8d4bd2cb73b599c956756693cf48f24f84
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-14 15:17:26 -0300

    Exercicio 1036

---
+++ src/exercicios/operacoesMatematicas/exe1036.java
@@ -0,0 +1,24 @@
+package exercicios.operacoesMatematicas;
+
+import java.util.Scanner;
+
+public class exe1036 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        if (a == 0) System.out.printf("Impossivel calcular%n");
+        else{
+            double delta = Math.pow(b, 2) - (4*a*c);
+
+            double x1 = (-b + (Math.sqrt(delta))) / (2*a);
+            double x2 = (-b - (Math.sqrt(delta))) / (2*a);
+
+            System.out.printf("R1 = %.5f%n", x1);
+            System.out.printf("R2 = %.5f%n", x2);
+        }
+    }
+}
