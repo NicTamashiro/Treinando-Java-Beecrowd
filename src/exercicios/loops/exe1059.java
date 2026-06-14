afe2b29802fe32dabf290a2331e8ef07c73d3148
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-14 17:01:35 -0300

    exercicio 1059

---
+++ src/exercicios/loops/exe1059.java
@@ -0,0 +1,9 @@
+package exercicios.loops;
+
+public class exe1059 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++){
+            if(i % 2 == 0) System.out.println(i);
+        }
+    }
+}
