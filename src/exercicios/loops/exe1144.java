d65cde139abefb6ec42b23835203f8eb19c7926e
Nicolas Tamashiro <adilsonq1976@gmail.com>
2026-06-15 09:57:44 -0300

    exercicio 1144

---
+++ src/exercicios/loops/exe1144.java
@@ -0,0 +1,16 @@
+package exercicios.loops;
+
+import java.util.Scanner;
+
+public class exe1144 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            System.out.printf("%d %d %d%n", i, i*i, i*i*i);
+            System.out.printf("%d %d %d%n", i, (i*i)+1, (i*i*i)+1);
+        }
+    }
+}
